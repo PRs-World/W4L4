d34c0a188d9261d9beaee7357706990f122c0b27
Pat Ramses Rulete Labirua <patramseslab@yahoo.com>
2023-08-31 17:45:08 +1000

    Commit 5
    Commit 5 for 90 HD 08/31/23 17:44.

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
+    public Renderer rend;
     private int i = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(gameObject.name + ":" + i);   
+
+        if(i > 100)
+        {
+            rend.enabled = false;
+        }
     }
 }
