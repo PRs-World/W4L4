ea0f9af5f79916e18d3fdee1a0167e2805b2c143
Pat Ramses Rulete Labirua <patramseslab@yahoo.com>
2023-08-31 20:47:59 +1000

    Commit 7 I think
    Commit 7 of 100 HD 08/31/23 20:47.

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -6,22 +6,34 @@ public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
     private int i = 3;
+    private int n;
+
     // Start is called before the first frame update
     void Start()
     {
-        //rend = GetComponent<Renderer>();
+        if (gameObject.tag == "Blue")
+        {
+            n = Random.Range(150, 251);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         i++;
-        Debug.Log(gameObject.name + ":" + i);   
 
-        /*if(i > 100)
+        Debug.Log(gameObject.name + ":" + i);
+
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        else if (gameObject.tag == "Blue" && i == n)
         {
             rend.enabled = false;
-        }*/
+        }
+
+        //i++;
     }
 }
     
